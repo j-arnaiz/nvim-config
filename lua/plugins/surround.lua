b7442a6bcba2b36675fd9f090116f3d7d19c9906
jesus <jesus@samyroad.com>
2025-07-15 13:14:21 +0200

    Changed vim-surround to mini surround and map to ñ

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -2,8 +2,22 @@
 
 return {
   {
-    "tpope/vim-surround",
+    "echasnovski/mini.surround",
     -- vim-surround no suele necesitar una función 'config' a menos que
     -- quieras hacer algo muy específico, ya que funciona con mapeos predeterminados
+    opts = {
+      mappings = {
+        add = "ña", -- Add surrounding in Normal and Visual modes
+        delete = "ñd", -- Delete surrounding
+        find = "ñf", -- Find surrounding (to the right)
+        find_left = "ñF", -- Find surrounding (to the left)
+        highlight = "ñh", -- Highlight surrounding
+        replace = "ñr", -- Replace surrounding
+        update_n_lines = "ñn", -- Update `n_lines`
+
+        suffix_last = "l", -- Suffix to search with "prev" method
+        suffix_next = "n", -- Suffix to search with "next" method
+      },
+    },
   },
 }
