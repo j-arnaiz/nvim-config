6b9f61ae02189528be90521605ed226d3fd7455c
jesus <jesus@samyroad.com>
2026-04-21 09:13:25 +0200

    Disabled dashboard and some fixes

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   -- disable leap
+  { "nvimdev/dashboard-nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,6 +10,7 @@ return {
           },
         },
       },
+      dashboard = { enabled = false },
     },
   },
 }
